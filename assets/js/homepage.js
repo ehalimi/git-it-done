d3048f9a7acf4e4f3b08194456b65ddc1df8d0e9
Ema Halimi <ehalimi@na.edu>
2022-07-06 16:40:51 -0500

    added the search by featured repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,5 @@
 var userFormEl = document.querySelector('#user-form');
+var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
@@ -21,6 +22,18 @@ var formSubmitHandler = function(event) {
   }
 };
 
+var buttonClickHandler = function(event) {
+    // get the language attribute from the clicked element
+    var language = event.target.getAttribute("data-language");
+  
+    if (language) {
+      getFeaturedRepos(language);
+  
+      // clear old content
+      repoContainerEl.textContent = "";
+    }
+  };
+
 var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = 'https://api.github.com/users/' + user + '/repos';
@@ -41,6 +54,21 @@ var getUserRepos = function(user) {
     })
     .catch(function(error) {
       alert('Unable to connect to GitHub');
+    });
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        // request was successful
+        if (response.ok) {
+          response.json().then(function(data) {
+            displayRepos(data.items, language);
+          });
+        } else {
+          alert("Error: " + response.statusText);
+        }
     });
 };
 
@@ -90,5 +118,6 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
-// add event listeners to forms
+// add event listeners to form and button container
 userFormEl.addEventListener('submit', formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
